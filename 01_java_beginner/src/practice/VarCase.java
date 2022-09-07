f470c3d86581ba1dcdf4c9fc5f82a9816e37c230
giduthuris <giduthuri.srini@gmail.com>
2022-09-07 21:02:46 +0530

    added m1(int,int,int) function.

--- 01_java_beginner/src/practice/VarCase.java
+++ 01_java_beginner/src/practice/VarCase.java
@@ -1,10 +1,16 @@
 package practice;
 
 public class VarCase {
+	void m1(int a, int b, int c) {
+		System.out.println(a+" " + b + " " + c + " ");
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
+		byte b =10;
+		VarCase m = new VarCase();
+		m.m1(b, b, b);
 
 	}
 
