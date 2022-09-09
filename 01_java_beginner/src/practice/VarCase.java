eb3de1dd73a75c3bc6746804e72c07e1c92ff01c
giduthuris <giduthuri.srini@gmail.com>
2022-09-09 09:34:27 +0530

    Test class added

--- 01_java_beginner/src/practice/VarCase.java
+++ 01_java_beginner/src/practice/VarCase.java
@@ -4,13 +4,16 @@ public class VarCase {
 	void m1(int a, int b, int c) {
 		System.out.println(a+" " + b + " " + c + " ");
 	}
-
+	void m1(byte...b) {
+		System.out.println(b);
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
 		byte b =10;
 		VarCase m = new VarCase();
 		m.m1(b, b, b);
+		m.m1(b);
 
 	}
 
