54fbca37958d250f967ff9882c8e333563be1e48
giduthuris <giduthuri.srini@gmail.com>
2022-09-09 15:52:22 +0530

    added 3 more variables

--- 01_java_beginner/src/practice/Test.java
+++ 01_java_beginner/src/practice/Test.java
@@ -16,6 +16,10 @@ public class Test {
 		Boolean b4 = new Boolean("hey"); // false
 		Boolean b5 = new Boolean("Bye"); // false
 		System.out.println(b4.equals(b5)); // true
+		
+		Boolean b6 = new Boolean("hello"); // false
+		Boolean b7 = new Boolean(false); // false
+		System.out.println(b6.equals(b7)); // true
 	
 	}
 
